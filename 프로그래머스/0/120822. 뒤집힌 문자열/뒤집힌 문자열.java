b10857c72273a107c5f8e04a0e0e10fb22e9a89f
Ji_Jung_Ho <110534668+hoya-q@users.noreply.github.com>
2025-06-08 14:48:52 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 9.90 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -1,5 +1,9 @@
 class Solution {
     public String solution(String my_string) {
-        return new StringBuilder(my_string).reverse().toString();
+        String answer = "";
+        for (int i = my_string.length() - 1; i >= 0; i--) {
+            answer += my_string.charAt(i);
+        }
+        return answer;
     }
 }
